a5eaa64ac571ad13dc3151bcbcdaf107745ccc04
belkisarslan <belkisarslan33@gmail.com>
2022-11-17 21:17:38 +0300

    add component developed

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "../App.css"
 
 const Add = () => {
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([])
+
+  function onChange(e) {
+    setQuery(e.target.value)
+
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`).then((res) => res.json().then((data) => {
+      if (!data.errors) {
+        setResults(data.results);
+      } else {
+        setResults([]);
+      }
+    }))
+  }
   return (
-    <div>Add</div>
+    <>
+    <header className='masthead'>
+      <div className='container position-relative'>
+        <div className="row justify-content-center">
+          <div className="col-xl-6">
+            <div className="text-center text-white">
+              <h1 className="mb-5">Sen Hangilerini İzledin?</h1>
+              <div className='row'>
+                <div className='col'>
+                  <input value={query} onChange={onChange} className="form-control form-control-lg" type="text" placeholder='Film, dizi, oyuncu ara...' />
+                </div>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </header >
+      {results.map((movie)=>(
+        <p>{movie.title}</p>
+      ))}
+ </>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import '../App.css'
 
 const Header = () => {
+   
     return (
         <>
             <nav className="navbar navbar-light bg-light static-top">
@@ -23,25 +24,8 @@ const Header = () => {
                     </ul>
                 </div>
             </nav>
-            <header className='masthead'>
-                <div className='container position-relative'>
-                    <div className="row justify-content-center">
-                        <div className="col-xl-6">
-                            <div className="text-center text-white">
-                                <h1 className="mb-5">Sen Hangilerini İzledin?</h1>
-                                <div className='row'>
-                                    <div className='col'>
-                                        <input className="form-control form-control-lg" type="text" />
-                                    </div>
-                                    <div className="col-auto">
-                                        <button className="btn btn-primary btn-lg disabled" id="submitButton" type="submit">Ara</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </header >
+           
+           
         </>
     )
 }
