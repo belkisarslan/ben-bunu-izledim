d330d5294c14e5505d584ab134cb5f339dbb2de5
belkisarslan <belkisarslan33@gmail.com>
2022-11-19 17:34:04 +0300

    local storage added

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,15 +1,20 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import AppReducer from "./AppReducer";
 
 export const GlobalContext = createContext()
 
 const initialState = {
-  watchList: [],
+  watchList: localStorage.getItem("watchList") ? JSON.parse(localStorage.getItem("watchList")) : [],
   watched: []
 }
 
 export const GlobalProvider =(props) => {
   const [state, dispatch] = useReducer(AppReducer, initialState)
+
+  useEffect(() => {
+    localStorage.setItem("watchList", JSON.stringify(state.watchList))
+  }, [state])
+  
   
   const addMovieToWatchList = (movie) => {
     dispatch({type: "ADD_MOVİE_TO_WATCHLİST", payload: movie})
