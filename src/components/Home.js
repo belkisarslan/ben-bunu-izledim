8db18c4da3f212acd678c73e4d0bcfec3e7523c0
belkisarslan <belkisarslan33@gmail.com>
2022-11-22 21:48:01 +0300

    validateDomNesting warning and className warning fixed

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,17 +4,17 @@ import "../App.css"
 
 const Home = () => {
   return (
-    <section class="page-section clearfix">
-    <div class="container">
-        <div class="intro">
-            <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="https://picsum.photos/id/1078/800/400" alt="..." />
-            <div class="intro-text left-0 text-center p-5 rounded">
-                <h2 class="section-heading mb-4">
-                    <span class="section-heading-upper">Ben Bunu İzledim'e</span>
-                    <span class="section-heading-lower">Hoş Geldiniz!</span>
+    <section className="page-section clearfix">
+    <div className="container">
+        <div className="intro">
+            <img className="intro-img img-fluid mb-3 mb-lg-0 rounded" src="https://picsum.photos/id/1078/800/400" alt="..." />
+            <div className="intro-text left-0 text-center p-5 rounded">
+                <h2 className="section-heading mb-4">
+                    <span className="section-heading-upper">Ben Bunu İzledim'e</span>
+                    <span className="section-heading-lower">Hoş Geldiniz!</span>
                 </h2>
-                <p class="mb-3">İzlediğiniz ya da izlemek istediğiniz filmleri listeleyin</p>
-                <Link to={"/Add"}><div class="intro-button mx-auto"><a class="btn btn-secondary btn-xl" href="#!">Film listeni oluşturmaya başla!</a></div></Link>
+                <p className="mb-3">İzlediğiniz ya da izlemek istediğiniz filmleri listeleyin</p>
+                <Link to={"/Add"}><div className="intro-button mx-auto"><div className="btn btn-secondary btn-xl" href="#!">Film listeni oluşturmaya başla!</div></div></Link>
             </div>
         </div>
     </div>
